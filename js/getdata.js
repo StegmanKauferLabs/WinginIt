998d50dc0827689761dc870bf0b56b16d736b840
Pierce Stegman <pwstegman@gmail.com>
2015-12-08 13:22:53 -0500

    Parsed data

--- js/getdata.js
+++ js/getdata.js
@@ -18,28 +18,43 @@ function loadURL(url, doneCallback) {
 
 function getData(callback){
 	var returnedCount = 0;
-	var xyURL = "res/xy00009.txt";
-	var elURL = "res/el00009.txt";
+	var xyURL = "/WinginIt/res/xy00009.txt";
+	var elURL = "/WinginIt/res/el00009.txt";
+	var xyList = [];
+	var elList = [];
+	var error = false;
 	loadURL(xyURL, function(xy){
 		if(xy === null){
-			console.log("ERROR");
+			error = true;
 			return;
 		}
-		console.log(xy);
+		xy = xy.split("\n");
+		var coords;
+		for(var i=1; i<xy.length; i++){
+			coords = xy[i].trim().split(/\s*/g);
+			xyList.push([coords[1], coords[2]])
+		}
+		console.log(xyList);
 		returnedCount += 1;
 		if(returnedCount == 2){
-			callback();
+			callback(xyList, elList, error);
 		}
 	});
 	loadURL(elURL, function(el){
 		if(xy === null){
-			console.log("ERROR");
+			error = true;
 			return;
 		}
-		console.log(el);
+		el = el.split("\n");
+		var coords;
+		for(var i=1; i<el.length; i++){
+			coords = el[i].trim().split(/\s*/g);
+			xyList.push(coords)
+		}
+		console.log(elList);
 		returnedCount += 1;
 		if(returnedCount == 2){
-			callback();
+			callback(xyList, elList, error);
 		}
 	});
 }
