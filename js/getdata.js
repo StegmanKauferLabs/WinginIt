fc4fd401ba086ac964e92e69d34b934306fba3b4
Pierce Stegman <pwstegman@gmail.com>
2015-12-08 13:37:18 -0500

    Finished get data

--- js/getdata.js
+++ js/getdata.js
@@ -18,10 +18,10 @@ function loadURL(url, doneCallback) {
 
 function getData(callback){
 	var returnedCount = 0;
-	var xyURL = "/WinginIt/res/xy00009.txt";
-	var elURL = "/WinginIt/res/el00009.txt";
-	var xyList = [];
-	var elList = [];
+	var xyURL = "res/xy00009.txt";
+	var elURL = "res/el00009.txt";
+	xyList = [];
+	elList = [];
 	var error = false;
 	loadURL(xyURL, function(xy){
 		if(xy === null){
@@ -31,30 +31,41 @@ function getData(callback){
 		xy = xy.split("\n");
 		var coords;
 		for(var i=1; i<xy.length; i++){
-			coords = xy[i].trim().split(/\s*/g);
-			xyList.push([coords[1], coords[2]])
+			coords = xy[i].trim();
+			coords = coords.split(/ {1,}/);
+			coords = coords.map(Number);
+			if(coords.length == 3){
+				xyList.push([coords[1], coords[2]]);
+			}
 		}
-		console.log(xyList);
 		returnedCount += 1;
 		if(returnedCount == 2){
 			callback(xyList, elList, error);
 		}
 	});
 	loadURL(elURL, function(el){
-		if(xy === null){
+		if(el === null){
 			error = true;
 			return;
 		}
 		el = el.split("\n");
 		var coords;
 		for(var i=1; i<el.length; i++){
-			coords = el[i].trim().split(/\s*/g);
-			xyList.push(coords)
+			coords = el[i].trim().split(/ {1,}/);
+			coords = coords.map(Number);
+			if(coords.length == 4){
+				elList.push(coords)
+			}
 		}
-		console.log(elList);
 		returnedCount += 1;
 		if(returnedCount == 2){
 			callback(xyList, elList, error);
 		}
 	});
 }
+
+getData(function(a, b, c){
+	console.log(a);
+	console.log(b);
+	console.log(c);
+});
